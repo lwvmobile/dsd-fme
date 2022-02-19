48c9ae4b285089cb0a8d03ce80adaeaf806bd6ed
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2022-02-19 06:20:19 -0500

    disable FM_banner
    disable FM_banner on ncurses terminal, still can't figure out ncursesw support in cmake

--- src/dsd_ncurses.c
+++ src/dsd_ncurses.c
@@ -104,7 +104,7 @@ char * getTimeN(void) //get pretty hh:mm:ss timestamp
 void ncursesOpen ()
 {
   mbe_printVersion (versionstr);
-  setlocale(LC_ALL, "");
+  //setlocale(LC_ALL, "");
   initscr(); //Initialize NCURSES screen window
   start_color();
   init_pair(1, COLOR_YELLOW, COLOR_BLACK);      //Yellow/Amber for frame sync/control channel, NV style
@@ -132,15 +132,15 @@ ncursesPrinter (dsd_opts * opts, dsd_state * state)
   level = 0; //start each cycle with 0
   erase();
   //disabling until wide support can be built for LM, etc. $(ncursesw5-config --cflags --libs)
-  printw ("%s \n", FM_bannerN[0]); //top line in white
+  //printw ("%s \n", FM_bannerN[0]); //top line in white
   attron(COLOR_PAIR(4));
-  for (short int i = 1; i < 7; i++) //following lines in cyan
-  {
-    printw("%s \n", FM_bannerN[i]);
-  }
+  //for (short int i = 1; i < 7; i++) //following lines in cyan
+  //{
+    //printw("%s \n", FM_bannerN[i]);
+  //}
   attroff(COLOR_PAIR(4));
   printw ("--Build Info------------------------------------------------------------------\n");
-  printw ("| %s \n", FM_bannerN[7]); //http link
+  //printw ("| %s \n", FM_bannerN[7]); //http link
   printw ("| Digital Speech Decoder: Florida Man Edition\n");
   printw ("| Github Build Version: %s \n", GIT_TAG);
   printw ("| mbelib version %s\n", versionstr);
