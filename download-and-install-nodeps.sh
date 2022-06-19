634eba522fba2d4e66373140b7d13b6a1c70d95a
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2022-06-19 09:37:55 -0400

    Create download-and-install-nodeps.sh
    This version is to download and install if you already have dependencies installed, saving time over the initial download-and-install.sh which is only for new/clean builds without deps installed.

---
+++ download-and-install-nodeps.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+clear
+echo DSD-FME Digital Speech Decoder - Florida Man Edition Auto Installer 
+echo This will clone, build, and install DSD-FME only.
+echo If you need dependencies installed, please run download-and-install.sh instead. 
+echo
+
+read -p "Press enter to continue"
+
+git clone https://github.com/lwvmobile/dsd-fme
+cd dsd-fme
+git branch -a
+git checkout remotes/origin/pulseaudio
+git checkout -b pulseaudio
+git branch -a #double check to see if you are on pulseaudio branch
+mkdir build
+cd build
+cmake ..
+make -j `nproc`
+##only run make install if you don't have another version already installed##
+sudo make install
+sudo ldconfig
