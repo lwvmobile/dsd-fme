21570b1d717ccecbaa000da353768a4e107a66ac
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2022-08-12 13:45:25 -0400

    Delete virtualsink.sh

--- virtualsink.sh
+++
@@ -1,4 +0,0 @@
-pacmd load-module module-null-sink sink_name=virtual_sink  sink_properties=device.description=Virtual_Sink
-pacmd load-module module-null-sink sink_name=virtual_sink2  sink_properties=device.description=Virtual_Sink2
-
-
