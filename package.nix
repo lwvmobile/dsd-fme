d9d6966f1e45b29b3f57ab2b3633cf39a6030f70
ChronosXYZ <chronosxyz000@gmail.com>
2024-03-24 04:06:15 +0300

    Add Nix flake for the project

---
+++ package.nix
@@ -0,0 +1,44 @@
+{ lib
+, stdenv
+, cmake
+, mbelib
+, libsndfile
+, ncurses
+, pulseaudio
+, rtl-sdr
+, itpp
+, portaudioSupport ? true
+, portaudio ? null
+}:
+
+assert portaudioSupport -> portaudio != null;
+
+stdenv.mkDerivation {
+  name = "dsd-fme";
+
+  src = ./.;
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [
+    mbelib
+    libsndfile
+    itpp
+    rtl-sdr
+    ncurses.dev
+    pulseaudio.dev
+  ] ++ lib.optionals portaudioSupport [ portaudio ];
+
+  doCheck = true;
+
+  meta = with lib; {
+    description = "Digital Speech Decoder - Florida Man Edition";
+    longDescription = ''
+      DSD is able to decode several digital voice formats from discriminator
+      tap audio and synthesize the decoded speech. Speech synthesis requires
+      mbelib, which is a separate package.
+    '';
+    homepage = "https://github.com/szechyjs/dsd";
+    license = licenses.mit;
+    platforms = platforms.unix;
+  };
+}
