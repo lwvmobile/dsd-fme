2ca3f0a3567af528884a31dce3c5361ea90b5126
lwvmobile <lwvmobile@gmail.com>
2025-07-23 19:16:27 -0400

    NXDN: Fix Wformat Warnings; #299

--- src/nxdn_deperm.c
+++ src/nxdn_deperm.c
@@ -599,7 +599,7 @@ void nxdn_deperm_sacch2(dsd_opts * opts, dsd_state * state, uint8_t bits[60])
 			fprintf (stderr, "Scrambler; ");
 			state->nxdn_cipher_type = 1;
 			if (state->R != 0)
-				fprintf (stderr, "Key: %d; ", state->R);
+				fprintf (stderr, "Key: %lld; ", state->R);
 		}
 		else if (cipher != 0x00)
 		{
@@ -640,7 +640,7 @@ void nxdn_deperm_sacch2(dsd_opts * opts, dsd_state * state, uint8_t bits[60])
 		{
 			fprintf (stderr, "\n DCR SFULL ");
 			for (int i = 0; i < 9; i++)
-				fprintf (stderr, "[%02X]", convert_bits_into_output(state->dmr_pdu_sf[0]+(i*8), 8));
+				fprintf (stderr, "[%02llX]", convert_bits_into_output(state->dmr_pdu_sf[0]+(i*8), 8));
 		}
 
 	}
